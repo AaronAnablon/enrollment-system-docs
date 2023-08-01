5e58d694debab9abc801760f24e0b476f1a9b4d9
AaronAnablon <anablonaaronp@gmail.com>
2023-08-01 18:49:28 +0800

    updated the Navbar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -15,11 +15,11 @@ const Layout = ({ children }) => {
     }
     return (
         <div className="grid grid-cols-4 block">
-           <div className="fixed top-0 lg:hidden w-screen block bg-gray-800 z-10">
-                 <div onClick={handleClick} className="m-4 text-white"><GiHamburgerMenu size={32}/></div>
+            <div className="fixed top-0 lg:hidden w-screen block bg-gray-800 z-10">
+                <div onClick={handleClick} className="m-4 text-white"><GiHamburgerMenu size={32} /></div>
             </div>
-            <div className={`lg:col-span-1 lg:block ${!menu ? 'hidden': 'fixed top-0 z-50 bg-white'} col-span-4`}>
-                <Sidebar handleClick={handleClick} menu={menu}/>
+            <div className={`lg:col-span-1 lg:block ${!menu ? 'hidden' : 'fixed top-0 z-50 bg-white'} col-span-4`}>
+                <Sidebar handleClick={handleClick} menu={menu} />
             </div>
             <div className="lg:col-span-3 lg:mt-0 mt-16 col-span-4 lg:mx-8 mx-2">{children ? children :
                 <div className="flex flex-col mt-10"><h2 className="text-4xl indent-4">{remarks.header}</h2>
@@ -28,13 +28,34 @@ const Layout = ({ children }) => {
                         <Nextbutton link='/pages/introduction' name='Introduction' />
                     </div>
                     <div className="flex flex-col items-center">
-                        <Image src={remarks.pic1} alt='pic1' />
-                        <Image src={remarks.pic2} alt='pic2' />
-                        <Image src={remarks.pic3} alt='pic3' />
-                        <Image src={remarks.pic4} alt='pic4' />
-                    </div>
-                    <div className="flex justify-end ">
-
+                        <Image
+                            src={remarks.pic1}
+                            alt="pic1"
+                            width={100} 
+                            height={100} 
+                            className="rounded-full border-2 border-white"
+                        />
+                        <Image
+                            src={remarks.pic2}
+                            alt="pic2"
+                            width={100} 
+                            height={100} 
+                            className="rounded-full border-2 border-white"
+                        />
+                        <Image
+                            src={remarks.pic3}
+                            alt="pic3"
+                            width={100} 
+                            height={100} 
+                            className="rounded-full border-2 border-white"
+                        />
+                        <Image
+                            src={remarks.pic4}
+                            alt="pic4"
+                            width={100} 
+                            height={100} 
+                            className="rounded-full border-2 border-white"
+                        />
                     </div>
                 </div>
             }
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { BsXLg } from "react-icons/bs";
 
 const Sidebar = ({ handleClick, menu }) => {
     return (
-        <div onClick={handleClick} className="h-screen sticky top-0 flex flex-col w-full z-50">
+        <div onClick={menu && handleClick} className="h-screen sticky top-0 flex flex-col w-full z-50">
             {menu && <div className='flex justify-end p-4 text-white bg-gray-800' ><BsXLg size={32} /></div>}
             <Link href={'/'}><h2 className="text-4xl text-white p-4 bg-gray-800">Enrollment System Documentation</h2></Link>
             <div className='p-4 flex flex-col text-bold text-xl border-r-2 text-gray-800 gap-4 '>
